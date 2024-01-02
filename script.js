0c6140aceae4b1e3542319e3acdc3c3d650be69a
Anshika Bundela <99082647+LAV1E@users.noreply.github.com>
2024-01-02 10:06:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-function firstWord(s) {
+function firstWord(str) {
   // your code here
+	 if (str.length === 0) {
+        return "";
+    }
+
+    // Find the index of the first space
+    var spaceIndex = str.indexOf(' ');
+
+    // If there is no space, return the entire string
+    if (spaceIndex === -1) {
+        return str;
+    }
+
+    // Otherwise, return the substring from the start to the first space
+    return str.substring(0, spaceIndex);
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
